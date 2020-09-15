698a0eb2467c10b6bb39a121f4c94281b7212f7b
lzcdev <lzc2134@gmail.com>
2020-09-16 00:46:12 +0800

    feat: add Hooks

--- react-single-01/src/pages/app.js
+++ react-single-01/src/pages/app.js
@@ -1,15 +1,25 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import './app.scss'
 import { Button } from 'antd'
 import 'antd/dist/antd.css'
 
 export default function App () {
-  return <div class="container">
+  const [count, setCount] = useState(10)
+
+  useEffect(() => {
+    console.log('执行了useEffect');
+  }, [])
+  return <div className="container">
     <h1>欢迎来到腾讯课堂</h1>
     <Link to="/login">点击跳转到Login页面</Link>
     <br />
     <Link to="/home">点击跳转到Home页面</Link>
+    <br />
+    <p>
+      当前count次数：{count}
+    </p>
+    <Button onClick={() => { setCount(count + 1) }}>更新次数</Button>
   </div>
 }
 // 有状态的组件
--- react-single-01/src/pages/detail.js
+++ react-single-01/src/pages/detail.js
@@ -1,10 +1,18 @@
 import React from "react";
-// import { Link } from "react-router-dom";
+import { Link, useParams, useHistory } from "react-router-dom";
 import './app.scss'
-
+import { Button } from "antd";
+import 'antd/dist/antd.css'
 
 export default function Detail () {
-  return <div class="container">
-    This is Detail
+  const params = useParams()
+  const history = useHistory()
+  return <div className="container">
+    <p>This is Detail</p>
+    <p>当前参数为:{params.id}</p>
+
+    <Button onClick={() => {
+      history.push('/')
+    }}>跳转首页</Button>
   </div>
 }
